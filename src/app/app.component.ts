496bbf0deaaf011a8d770f9f43156e594793d932
AshantyAlisson <alisson.ashanty.100@gmail.com>
2022-01-08 20:10:48 -0600

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { setOptions, MbscEventcalendarView, MbscCalendarEvent , localeEs } from '@mobiscroll/angular';
+import { HttpClient } from '@angular/common/http';
+
+setOptions({
+  locale: localeEs,
+  theme: 'ios',
+  themeVariant: 'light',
+  clickToCreate: false,
+  dragToCreate: false,
+  dragToMove: false,
+  dragToResize: false
+});
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'calendario';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+
+  myEvents: MbscCalendarEvent[] = [];
+  view = 'month';
+  calView: MbscEventcalendarView = {
+    calendar: { type: 'month' },
+    agenda: { type: 'month' }
+  };
+
+  ngOnInit(): void {
+    this.http.jsonp < MbscCalendarEvent[] > ('https://trial.mobiscroll.com/events/?vers=5', 'callback').subscribe((resp) => {
+      this.myEvents = resp;
+    });
+  }
+
+  changeView(): void {
+    setTimeout(() => {
+      switch (this.view) {
+        case 'month':
+          this.calView = {
+            calendar: { type: 'month' },
+            agenda: { type: 'month' }
+          };
+          break;
+        case 'week':
+          this.calView = {
+            calendar: { type: 'week' },
+            agenda: { type: 'week' }
+          };
+          break;
+        case 'day':
+          this.calView = {
+            agenda: { type: 'day' }
+          };
+          break;
+      }
+    });
+  }
 }
+
